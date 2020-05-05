15138dd21b2dafd5361fbe506f86ea6db4824fe4
Horki <ivanzvonimirhorvat@gmail.com>
2020-05-05 16:05:43 +0200

    test: separate packages

--- src/sedgewick/coursera/week0/tests/BinSearchTest.java
+++ src/sedgewick/coursera/week0/tests/BinSearchTest.java
@@ -1,8 +1,7 @@
-package sedgewick.coursera.week0.libs;
+package sedgewick.coursera.week0.tests;
 
 import org.junit.Assert;
-
-import static org.junit.Assert.*;
+import sedgewick.coursera.week0.libs.BinSearch;
 
 public class BinSearchTest {
 
@@ -13,6 +12,7 @@ public class BinSearchTest {
         int r1 = binSearch.bin_search(nums, 6);
         Assert.assertEquals(r1, 5);
     }
+
     @org.junit.Test
     public void bin_search_fails() {
         int nums[] = {1, 2, 3, 4, 5, 6};
