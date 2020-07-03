600d151f4f002c8c44b1333899bf549ac0d79a57
Horki <ivanzvonimirhorvat@gmail.com>
2020-07-03 19:01:25 +0200

    week4: bonus; frequenct counter

---
+++ src/sedgewick/coursera/week4/bonus/FrequencyCounter.java
@@ -0,0 +1,32 @@
+package sedgewick.coursera.week4.bonus;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class FrequencyCounter {
+    // tinyTale.txt
+    public static void main(String[] args) {
+        int minlen = 3;
+        ST<String, Integer> st = new ST<>();
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (word.length() < minlen) {
+                // ignore short strings
+                continue;
+            }
+            if (!st.contains(word)) {
+                st.put(word, 1);
+            } else {
+                st.put(word, st.get(word) + 1);
+            }
+        }
+        String max = "";
+        st.put(max, 0);
+        for (String word : st.keys()) {
+            if (st.get(word) > st.get(max)) {
+                max = word;
+            }
+        }
+        StdOut.println(max + ": " + st.get(max));
+    }
+}
