6bfbafcf32ad1eda05ba716dfee76281c1ab6496
Horki <ivanzvonimirhorvat@gmail.com>
2020-09-17 12:28:23 +0200

    week0: main; linter fix

--- src/sedgewick/coursera/week0/Main.java
+++ src/sedgewick/coursera/week0/Main.java
@@ -4,7 +4,7 @@ import sedgewick.coursera.week0.libs.BinSearch;
 
 public class Main {
     public static void main(String[] args) {
-        int a[] = {1, 2, 3, 4, 5, 6};
+        int[] a = {1, 2, 3, 4, 5, 6};
         BinSearch binSearch = new BinSearch();
         int k = binSearch.bin_search(a, 6);
         System.out.println("result = " + k);
