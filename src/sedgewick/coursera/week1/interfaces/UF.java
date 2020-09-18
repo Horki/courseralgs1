c3ddddad12622fbb21f2a5a40c67bb8e52a06895
Horki <ivanzvonimirhorvat@gmail.com>
2020-09-18 15:00:30 +0200

    week1: interface Uf; remove redudnant public in interface

--- src/sedgewick/coursera/week1/interfaces/UF.java
+++ src/sedgewick/coursera/week1/interfaces/UF.java
@@ -1,9 +1,9 @@
 package sedgewick.coursera.week1.interfaces;
 
 public interface UF<T> {
-    public void union(T p, T q);
-    public boolean connected(T p, T q);
-    public T find(T p);
-    public int count();
-    public void debug_output();
+    void union(T p, T q);
+    boolean connected(T p, T q);
+    T find(T p);
+    int count();
+    void debug_output();
 }
